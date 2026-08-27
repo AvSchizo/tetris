a60500949bb3808150452d82a5cee355a281d8ea
Paul Kolz <paul.d.kolz@gmail.com>
2026-08-26 22:36:22 -0500

    setup

--- Main-Window.py
+++ Main-Window.py
@@ -1,13 +1,32 @@
 import pygame
+from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+
+FPS = 60
+currentFrame = 0
+
 while running:
-	# poll for events
-	# pygame.QUIT event means the user clicked X to close your window
+
+	currentFrame += 1
+
+
+
 	for event in pygame.event.get():
+
+
 		if event.type == pygame.QUIT:
+
 			running = False
+			exit()
+
+
+
+
+
+
+	clock.tick(60)
