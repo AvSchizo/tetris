b696f808ca93e3080c9d64655ef2b9815e985868
Jane Doe <paul.d.kolz@gmail.com>
2026-08-28 14:37:47 -0500

    Update Main-Window.py

--- Main-Window.py
+++ Main-Window.py
@@ -151,8 +151,13 @@ playerInputs = {
 	"rotLeft": pygame.K_q,
 	"rotRight": pygame.K_e,
 	"hold": pygame.K_SPACE,
-	"enter": pygame.K_ENTER,
+	"enter": pygame.K_RETURN,
 	"escape": pygame.K_ESCAPE,
+	"mouse_x": pygame.mouse.get_pos()[0],
+	"mouse_y": pygame.mouse.get_pos()[1],
+	"mouseButton_L": pygame.mouse.get_pressed()[0],
+	"mouseButton_M": pygame.mouse.get_pressed()[1],
+	"mouseButton_R": pygame.mouse.get_pressed()[2],
 }
 
 FPS = 60
